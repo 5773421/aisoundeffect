40c81995978a2f2c515db60662f7ad994d9ca12a
mhh <3122215524@qq.com>
2024-06-09 02:02:44 +0800

    fix: del pricing

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
         <IframeCom />
         {/* <Problem /> */}
         <FeaturesAccordion />
-        <Pricing />
+        {/* <Pricing /> */}
         <HowToUse />
         <FAQ />
         {/* <CTA /> */}
