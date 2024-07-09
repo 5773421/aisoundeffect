3461db4df6cfaede64bc4e36b0c23a0074122e56
mhh <3122215524@qq.com>
2024-07-10 00:37:46 +0800

    fix: bugfix

--- contexts/AppContext.tsx
+++ contexts/AppContext.tsx
@@ -7,19 +7,19 @@ export const AppContextProvider = ({ children }: any) => {
   const [credits, setCredits] = useState<any | null | undefined>(undefined);
   const session = useSession();
   const fetchUserCredits = async function () {
-    // try {
-    //   if (!session || (JSON.stringify(session) === '{}')) {
-    //     // 未登陆，跳转登陆
-    //     return;
-    //   }
-    //   const ret: any = await apiClient.get(
-    //     "/get-credits",
-    //   );
-    //   setCredits(ret.data);
-    // } catch (e) {
-    //   setCredits(null);
-    //   console.log("get user credits failed: ", e);
-    // }
+    try {
+      if (session.status !== 'authenticated') {
+        // 未登陆，跳转登陆
+        return;
+      }
+      const ret: any = await apiClient.get(
+        "/get-credits",
+      );
+      setCredits(ret.data);
+    } catch (e) {
+      setCredits(null);
+      console.log("get user credits failed: ", e);
+    }
   };
 
   useEffect(() => {
