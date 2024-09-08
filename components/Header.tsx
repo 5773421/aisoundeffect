c557b972ec0024dd8e97eac29661d7ff8827e9c3
mhh <3122215524@qq.com>
2024-09-08 18:35:20 +0800

    fix: delete langechange

--- components/Header.tsx
+++ components/Header.tsx
@@ -47,7 +47,7 @@ const links: {
   },
 ];
 
-const cta: JSX.Element = <><LangSwitcher /><ButtonSignin extraStyle="btn-primary" /></>;
+const cta: JSX.Element = <><ButtonSignin extraStyle="btn-primary" /></>;
 
 // A header with a logo on the left, links in the center (like Pricing, etc...), and a CTA (like Get Started or Login) on the right.
 // The header is responsive, and on mobile, the links are hidden behind a burger button.
